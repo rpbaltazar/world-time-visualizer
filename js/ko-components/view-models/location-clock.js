1b3516134d75bc6066a35b434d984f96de83f72b
Rui Baltazar <rui.p.baltazar@gmail.com>
2015-05-29 11:41:56 +0800

    Added time indication in the clock

--- js/ko-components/view-models/location-clock.js
+++ js/ko-components/view-models/location-clock.js
@@ -17,28 +17,41 @@
     }
 
     LocationClock.prototype.updateTime = function() {
-      var canvas, context, counterClockwise, endAngle, hourShift, i, j, mCurrentTime, radius, results, startAngle, x, y;
+      var angle, canvas, context, counterClockwise, currentHour, endAngle, hourShift, hourText, i, j, mCurrentTime, outerRadius, px, py, radius, results, startAngle, x, y;
       mCurrentTime = moment.tz(this.timezone());
       this.currentTime(mCurrentTime.toString());
       hourShift = mCurrentTime.hour();
       canvas = document.getElementById(this.id());
-      console.log(canvas);
       if (canvas) {
         context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
         x = canvas.width / 2;
         y = canvas.height / 2;
         radius = 40;
+        outerRadius = radius + 14;
         results = [];
         for (i = j = 0; j <= 23; i = ++j) {
-          startAngle = (hourShift + i - 0.5) * 2 * Math.PI / 24;
-          endAngle = (hourShift + i + 0.5) * 2 * Math.PI / 24;
+          startAngle = (i - 0.5) * 2 * Math.PI / 24 - (Math.PI / 2);
+          endAngle = (i + 0.5) * 2 * Math.PI / 24 - (Math.PI / 2);
+          angle = 2 * Math.PI - ((endAngle - startAngle) / 2 + startAngle) + Math.PI / 2;
           counterClockwise = false;
           context.beginPath();
           context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
           context.lineWidth = 15;
           context.closePath();
-          context.strokeStyle = this.colorArray[i];
-          results.push(context.stroke());
+          if (hourShift + i > 24) {
+            currentHour = hourShift + i - 24;
+          } else {
+            currentHour = hourShift + i;
+          }
+          context.strokeStyle = this.colorArray[currentHour];
+          context.stroke();
+          context.font = '12 serif';
+          hourText = context.measureText(currentHour);
+          px = Math.sin(angle) * outerRadius + x - hourText.width / 2;
+          py = Math.cos(angle) * outerRadius + y + 3;
+          context.fillStyle = this.colorArray[currentHour];
+          results.push(context.fillText(currentHour, px, py));
         }
         return results;
       }
